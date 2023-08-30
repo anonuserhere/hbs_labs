2ed911c2a55f75286d2c63a42724dcc6710d96a8
anon user <ben.gamepro@gmail.com>
2023-08-30 06:57:16 +0000

    up to create

--- routes/poster.js
+++ routes/poster.js
@@ -1,13 +1,50 @@
-const express = require("express")
-const router = express.Router()
-const { Poster } = require("../models")
+const express = require("express");
+const router = express.Router();
+const { Poster } = require("../models");
+const { bootstrapField, createProductForm } = require("../forms");
 
 router.get("/", async (req, res) => {
-    let posters = await Poster.collection().fetch()
-    res.render("posters/index", {
-        posters: posters.toJSON()
-    })
+  let posters = await Poster.collection().fetch();
+  res.render("posters/index", {
+    posters: posters.toJSON(),
+  });
+});
 
+router.get("/create", async (req, res) => {
+  const productForm = createProductForm();
+  res.render("posters/create", {
+    form: productForm.toHTML(bootstrapField),
+  });
+});
+
+router.post("/create", async (req, res) => {
+  const productForm = createProductForm();
+  productForm.handle(req, {
+    success: async (form) => {
+      const poster = new Poster();
+      poster.set("title", form.data.name);
+      poster.set("description", form.data.description);
+      poster.set("price", form.data.cost);
+      await poster.save();
+      res.redirect("/posters");
+    },
+  });
+});
+
+router.post("/", async (req, res) => {
+  console.log("post /create");
+  productForm.handle(req, {
+    "success": async (form) => {
+      const product = new Poster(form.data);
+      await product.save();
+      res.redirect("/posters");
+    },
+    "error": async (form) => {
+      res.render("posters/create"), {
+        form: form.toHTML(bootstrapField)
+      }
+    }
+  });
 })
 
-module.exports = router
+module.exports = router;
